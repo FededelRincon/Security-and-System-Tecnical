d019340ebf2a37366e3116a4a933224b9138c553
FededelRincon <delrincon@gmail.com>
2024-07-03 11:15:50 -0300

    update css into media-queries desktop, get ready to do mobile

--- app/components/Sidebar/Sidebar.tsx
+++ app/components/Sidebar/Sidebar.tsx
@@ -3,9 +3,14 @@ import styles from "./Sidebar.module.css";
 export default function SidebarComponent() {
   return (
     <>
-      <div className={styles.sidebar}>
-        Sidebar
-      </div>
+      <aside className={styles.sidebar}>
+        <h3>Sidebar</h3>
+        <ul>
+          <li>Opción 1</li>
+          <li>Opción 2</li>
+          <li>Opción 3</li>
+        </ul>
+      </aside>
     </>
   )
 }
